6001c971fd151788638b8da06d400b1fe085ce98
aisana0131 <aisana0131@yahoo.com>
2026-01-09 15:22:31 -0600

    s3 bucket update:v2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "aws-session-s3-bucket-aisana"
-    key    = "/terraform/terraform.tfstate"
+    key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
 }
