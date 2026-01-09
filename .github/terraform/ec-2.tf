0f1c1a749a57076e2c363d55f45251144fb53013
aisana0131 <aisana0131@yahoo.com>
2026-01-09 14:43:55 -0600

    test workflow 3

--- .github/terraform/ec-2.tf
+++ .github/terraform/ec-2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "first_ec2" {
-  ami           = "ami-07ff62358b87c7116" // changed ami
+  ami           = "ami-07ff62358b87c7116" // changed ami to actual one
   instance_type = "t2.micro"
   tags = {
     Name        = "first"
     Environment = "dev"
   }
 }
+
 
 provider "aws" {
   region = "us-east-1"
