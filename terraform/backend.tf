88ea8c81a9440816c9d3d340b9997b7eeeb95fe1
aisana0131 <aisana0131@yahoo.com>
2026-01-12 14:02:40 -0600

    testing a new caller workflow for dev env

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ac-task-terraform-state"
-    key    = "ec2/terraform.tfstate"
+    key    = "final test/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "ac-task-terraform"
     encrypt        = true
