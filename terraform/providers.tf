a6d6ae373872d71aa602d7b06961115644b9c70f
aisana0131 <aisana0131@yahoo.com>
2026-01-12 19:30:11 -0600

    Checking okta integration:v4

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  required_providers {
-    okta = {
-      source  = "okta/okta"
-      version = "~> 4.10.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     okta = {
+#       source  = "okta/okta"
+#       version = "~> 4.10.0"
+#     }
+#   }
+# }
 
 # provider "okta" {
 #   org_name     = var.okta_org_name
